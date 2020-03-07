0332e92da3f03db5432b83031cb9331e40a911aa
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2020-03-07 21:54:54 +0530

    PORT added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express();
+var PORT = process.env.PORT || 3000;
 app.get('/',function(req,res){
     res.send('<h1>express works</h1>');
    });
-   app.listen(3000,function(){
+   app.listen(PORT,function(){
     console.log('Server listening to port 3000');
 });
