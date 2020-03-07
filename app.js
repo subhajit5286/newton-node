7b9b1989d366c6e4e0b4c5bc2aebf18d6e19e36a
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2020-03-07 21:35:19 +0530

    api added

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var express = require('express')
 var app = express();
+app.get('/',function(req,res){
+    res.send('<h1>express works</h1>');
+   });
+   app.listen(3000,function(){
+    console.log('Server listening to port 3000');
+});
